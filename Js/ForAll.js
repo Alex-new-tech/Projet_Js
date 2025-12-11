fbcbb1e6683a0187aeb503a6e2d6a4775ae0e640
Alexis <alexisbouille7@gmail.com>
2025-12-11 14:15:49 +0100

    Ajout de la gestion des films avec récupération des données de l'API, mise à jour de l'affichage des films dans la section des films tendances, et amélioration du style des sections.

--- Js/ForAll.js
+++ Js/ForAll.js
@@ -23,6 +23,46 @@ async function DonneeFilms(url) {
     }
 }
 
-const ToutFilm = DonneeFilms("https://www.omdbapi.com/?apikey=5e37588e&s=*");
 
+let spiderManFilms = [];
+let avengerFilms = [];
+let marvelFilms = [];
+let batmanFilms = [];
+let starWarsFilms = [];
+let harryPotterFilms = [];
+let fastAndFuriousFilms = [];
+let missionImpossible = [];
+let meilleursFilms = [];
 
+async function initialiserFilms(film,premierParametre = "s=") {
+    try {
+        let films = await DonneeFilms(`https://www.omdbapi.com/?apikey=5e37588e&${premierParametre}${film}`);
+        films = films.Search.filter(film => film.Type === "movie");
+        return films;
+    }   
+    catch (error) {
+        console.error("Erreur lors de l'initialisation des films :", error);
+        return [];  
+    }
+}
+
+async function main() {
+    spiderManFilms = await initialiserFilms("spider-man");
+    avengerFilms = await initialiserFilms("avengers");
+    marvelFilms = await initialiserFilms("marvel");
+    batmanFilms = await initialiserFilms("batman");
+    starWarsFilms = await initialiserFilms("Star Wars");
+    harryPotterFilms = await initialiserFilms("Harry Potter");
+    fastAndFuriousFilms = await initialiserFilms("Fast and Furious");
+    missionImpossible = await initialiserFilms("Mission Impossible");
+    console.log(spiderManFilms);
+    console.log(starWarsFilms);
+    console.log(fastAndFuriousFilms);
+    meilleursFilms.push (spiderManFilms.find(film => film.Title == "Spider-Man: No Way Home"));
+    meilleursFilms.push (starWarsFilms.find(film => film.Title == "Rogue One: A Star Wars Story"));
+    meilleursFilms.push (fastAndFuriousFilms.find(film => film.Title == "Fast and the Furious: Tokyo Drift - The Japanese Way"));
+    console.log(meilleursFilms);
+    document.dispatchEvent(new Event("films-prets"));
+}
+
+main();
