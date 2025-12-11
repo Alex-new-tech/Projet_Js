982e342825fea483575c621403461480ab23c8f8
Alexis <alexisbouille7@gmail.com>
2025-12-11 15:59:01 +0100

    MIse en place d'une fonction pour ajouter les films

--- Js/index.js
+++ Js/index.js
@@ -1,10 +1,7 @@
 let ChargerFilms = document.getElementById("ButtonChargerFilms");
 const SectionFilmsTendances = document.getElementById("FilmsTendances");
 
-document.addEventListener("films-prets", () => {
-    let filmImage = meilleursFilms[0].Poster;
-    let filmTitre = meilleursFilms[0].Title;
-    let filmDescription = meilleursFilms[0].Title;
+function ajoutFilmHtml(filmImage, filmTitre) {
     let div = document.createElement("div");
     div.innerHTML = `
     <div>
@@ -13,38 +10,29 @@ document.addEventListener("films-prets", () => {
         <a href=pages/movie.html> En Savoir plus </a>
     </div>`;
     SectionFilmsTendances.appendChild(div);
+};
+
+document.addEventListener("films-prets", () => {
+    let filmImage = meilleursFilms[0].Poster;
+    let filmTitre = meilleursFilms[0].Title;
+    ajoutFilmHtml(filmImage, filmTitre);
+
     filmImage = meilleursFilms[1].Poster;
     filmTitre = meilleursFilms[1].Title;
-    filmDescription = meilleursFilms[1].Title;
-    div = document.createElement("div");
-    div.innerHTML = `
-    <div>
-        <img src=${filmImage} alt="Poster du film ${filmTitre}">
-        <h3>${filmTitre}</h3>
-        <a href=pages/movie.html> En Savoir plus </a>
-    </div>`;
-    SectionFilmsTendances.appendChild(div);
+    ajoutFilmHtml(filmImage, filmTitre);
+
     filmImage = meilleursFilms[2].Poster;
     filmTitre = meilleursFilms[2].Title;
-    filmDescription = meilleursFilms[2].Title;
-    div = document.createElement("div");
-    div.innerHTML = `
-    <div>
-        <img src=${filmImage} alt="Poster du film ${filmTitre}">
-        <h3>${filmTitre}</h3>
-        <a href=pages/movie.html> En Savoir plus </a>
-    </div>`;
-    SectionFilmsTendances.appendChild(div);
+    ajoutFilmHtml(filmImage, filmTitre);
 });
 
 ChargerFilms.addEventListener("click", () => {
     const div = document.createElement("div");
     div.innerHTML = `
     <div>
-        <img src=${filmImage} alt="Film 3">
+        <img src=${filmImage} alt="Poster du film ${filmTitre}">
         <h3>${filmTitre}</h3>
-        <p>${filmDescription}</p>
+        <a href=pages/movie.html> En Savoir plus </a>
     </div>`;
     SectionFilmsTendances.appendChild(div);
 });
-
