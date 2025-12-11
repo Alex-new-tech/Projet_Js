ef4259db2871e01fae8a10de1158f508a69dca73
Alexis <alexisbouille7@gmail.com>
2025-12-11 09:15:09 +0100

    Changement du noms du css header footer changements dans les pages html du fichier link header footer

--- Js/ForAll.js
+++ Js/ForAll.js
@@ -11,3 +11,18 @@ if (nav.length > 0){
         });
     }
 }
+
+async function DonneeFilms(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data; 
+    } catch (error) {
+        console.error("Erreur lors de la récupération des données :", error);
+        throw error;
+    }
+}
+
+const ToutFilm = DonneeFilms("https://www.omdbapi.com/?apikey=5e37588e&s=*");
+
+
